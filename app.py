1cabb8b0c4ce508a8abd65ac136a602803a0f742
awbreemer <97578656+awbreemer@users.noreply.github.com>
2023-04-04 22:29:23 -0700

    fuck again

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/', methods = ['GET', 'POST'])
+def form():
+    if request.method == 'POST':
+        value = request.form['value']
+        #here is where somthing should be done with the value
+        return 'You entered: {}'.format(value)
+    return render_template('form.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host = '0.0.0.0')
