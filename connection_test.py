bed0de6932d67010c2894bc2f099f8b1ecc6765c
awbreemer <97578656+awbreemer@users.noreply.github.com>
2023-04-30 14:49:08 -0700

    fuck

--- connection_test.py
+++ connection_test.py
@@ -1,10 +1,12 @@
 import serial
+import time
 
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyUSB0', 9600, timeout = 1)
     ser.reset_input_buffer()
 
     while True:
-        if ser.in_waiting > 0:
-            line = ser.readline().decode('ascii').rstrip()
-            print(line)
+        ser.write(b"Hello from Raspberry Pi!\n")
+        line = ser.readline().decode('ascii').rstrip()
+        print(line)
+        time.sleep(1)
